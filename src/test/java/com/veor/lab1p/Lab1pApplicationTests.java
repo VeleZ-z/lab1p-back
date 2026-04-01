21192cfca15fece8b40070a88f4122b780c1dc5f
VelezZ <camilo.velez4@udea.edu.co>
2026-04-01 01:54:30 -0500

    GitHActions

--- src/test/java/com/veor/lab1p/Lab1pApplicationTests.java
+++ src/test/java/com/veor/lab1p/Lab1pApplicationTests.java
@@ -2,12 +2,13 @@ package com.veor.lab1p;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Lab1pApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
